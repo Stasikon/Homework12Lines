b67af26e046fcaf688dba9fe0339a710459bd01e
Stasikon <lecsa515@gmail.com>
2023-03-09 13:46:28 +0300

    Lessons 12 Lines

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1(){
+        System.out.println("hello world");
+    }
+    public static void task2(){
+
+    }
+    public static void task3(){
+
+    }
+    public static void task4(){
+
+    }
+}
